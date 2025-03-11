1cc81dbc81a0b093d6aa641074bdb2d9544454ca
xXErDaniXx06. <danielcarrascogarcia18@gmail.com>
2025-03-11 10:05:04 +0100

    Updated class Data with a new method

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -61,6 +61,10 @@ public class Date {
     this.year = year;
   }
 
+  public void Prueba(){
+    int c = 6;
+  }
+
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
    * month within 1 to 12, and the day valid within the given month and year, considering leap
